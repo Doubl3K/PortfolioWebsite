2e5dc915e440d85d0e89f22d8e52e40126e6c3f0
Doubl3K <kevin-kinner@web.de>
2022-07-18 21:39:50 +0200

    Layout fixes

--- logic.js
+++ logic.js
@@ -7,6 +7,7 @@ function scrollFunction() {
   } else {
     document.getElementById("header").style.top = "0px";
     document.getElementById("nav-wrapper").style.height = "10vh";
-
   }
 }
+
+console.error("Wenn sie schon einmal hier sind können sie auch gerne eine E-Mail an jobs@kevinkinner.de senden.");
