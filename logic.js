ebfc2fb869b692ee3f95a67b2e43dfa429c84a11
Doubl3K <kevin-kinner@web.de>
2022-07-22 15:43:44 +0200

    Fixed scrolling issue with topbar

--- logic.js
+++ logic.js
@@ -1,13 +1,12 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+  if (document.documentElement.scrollTop > 900) {
     document.getElementById("header").style.top = "-20px";
-    document.getElementById("nav-wrapper").style.height = "8vh";
-  } else {
+  } else{
     document.getElementById("header").style.top = "0px";
-    document.getElementById("nav-wrapper").style.height = "10vh";
   }
+  console.log('test');
 }
 
 console.error("Wenn sie schon einmal hier sind können sie auch gerne eine E-Mail an jobs@kevinkinner.de senden.");
