6837428e7cc6be79e7e1e0717eae58a67613f308
Kevin Kinner <kevin-kinner@web.de>
2022-09-06 10:24:42 +0200

    Now able to switch between themes endlessly

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+let darkmodeActive = true;
 let themeButton = document.getElementById("themechanger");
 themeButton.addEventListener("click", () => {
     let body = document.getElementsByTagName("body");
-    body[0].style.background = "hsl(150, 14%, 84%)";
-    body[0].style.color = "black";
+    
+    if (darkmodeActive == true) {
+        body[0].style.background = "hsl(150, 14%, 84%)";
+        body[0].style.color = "black";
+        themeButton.style.background = "black";
+        themeButton.style.color = "white";
+        darkmodeActive = false;
+    }
+    
+    else if (darkmodeActive == false) {
+        body[0].style.background = "black"
+        body[0].style.color = "white";
+        themeButton.style.background = "white";
+        themeButton.style.color = "black";
+        darkmodeActive = true;
+    }
 });
 
