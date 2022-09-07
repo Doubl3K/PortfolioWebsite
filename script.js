768bd77acf7b7a7ead21df8ecc407328ba28ccde
Kevin <kevin-kinner@web.de>
2022-09-07 16:53:35 +0200

    Themeswitcher done

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let darkmodeActive = true;
 let themeButton = document.getElementById("themechanger");
+let themeButtonCircle = document.getElementById("themechangercircle");
 themeButton.addEventListener("click", () => {
     let body = document.getElementsByTagName("body");
     
     if (darkmodeActive == true) {
         body[0].style.background = "hsl(150, 14%, 84%)";
         body[0].style.color = "black";
+        themeButton.style.background = "url(../Pictures/buttons/sunset.svg) no-repeat 90% 45%";
         themeButton.style.backgroundColor = "black";
         themeButton.style.color = "white";
+        themeButton.textContent = "Dark Mode";
         darkmodeActive = false;
     }
     
     else if (darkmodeActive == false) {
         body[0].style.background = "black"
         body[0].style.color = "white";
+        themeButton.style.background = "url(../Pictures/buttons/sunrise.svg) no-repeat 90% 45%";
         themeButton.style.backgroundColor = "white";
         themeButton.style.color = "black";
+        themeButton.textContent = "Light Mode"
         darkmodeActive = true;
     }
 });
