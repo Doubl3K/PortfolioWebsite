1a7b3f8cf2c6d2bb87c9d61faca329b54fd40395
Kevin <kevin-kinner@web.de>
2022-09-11 23:45:07 +0200

    Theme now Changes with animation

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 let darkmodeActive = true;
 let themeButton = document.getElementById("themechanger");
 let themeButtonCircle = document.getElementById("themechangercircle");
+let body = document.getElementsByTagName("body");
+
 themeButton.addEventListener("click", () => {
-    let body = document.getElementsByTagName("body");
     
     if (darkmodeActive == true) {
         body[0].style.background = "hsl(150, 14%, 84%)";
@@ -12,6 +13,7 @@ themeButton.addEventListener("click", () => {
         themeButton.style.backgroundColor = "black";
         themeButton.style.color = "white";
         themeButton.textContent = "Dark Mode";
+        waveAnimation(darkmodeActive);
         darkmodeActive = false;
     }
     
@@ -22,9 +24,50 @@ themeButton.addEventListener("click", () => {
         themeButton.style.backgroundColor = "white";
         themeButton.style.color = "black";
         themeButton.textContent = "Light Mode"
+        waveAnimation(darkmodeActive);
         darkmodeActive = true;
     }
 });
+
+function waveAnimation(darkmodeActive) {
+    let waveForm = document.createElement("div");
+    waveForm.style.width = "99vw";
+    waveForm.style.height = "300vh";
+    waveForm.style.borderRadius = "150px"
+    waveForm.style.zIndex = "-1";
+    waveForm.style.background = "black";
+    waveForm.style.position = "absolute";
+    waveForm.style.top = "0px";
+    waveForm.style.boxSizing = "border-box";
+    waveForm.style.overflow = "hidden";
+    if (darkmodeActive == true) {
+        waveForm.style.background = "black";
+    }
+    else if(darkmodeActive == false){
+        waveForm.style.background = "hsl(150, 14%, 84%)";
+    }
+
+    body[0].appendChild(waveForm);
+    let id = null;
+    let animationTopToBottom = ((id) =>{
+        let pos = 0;
+        clearInterval(id);
+        id = setInterval(frame, 15);
+        function frame (){
+            if (pos == 300 ) {
+                clearInterval(id);
+            }
+            else{
+                pos++;
+                waveForm.style.top = pos + "vh";
+                let height = 300 - pos;
+                waveForm.style.height = height + "vw";
+                console.log(height);
+
+            }
+        }
+    })();
+}
 
 //Periodic Headline Changer
 let abttxtc = document.getElementsByClassName("abouttext");
